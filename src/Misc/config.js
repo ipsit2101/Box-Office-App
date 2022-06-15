36872868a858f5ec3e6ea235460cbebac0829c1b
Ipsit <ishukr2101@gmail.com>
2022-06-15 19:08:29 +0530

    set search options

--- src/Misc/config.js
+++ src/Misc/config.js
@@ -3,7 +3,7 @@ const API_BASE_URL = "https://api.tvmaze.com";
 export async function apiGet(queryInput) {
 
   const response = await fetch(`${API_BASE_URL}${queryInput}`).then(  //This returns a promise which needs to be awaited
-    data => data.json()
+    data => data.json()       
   );
   return response;    // returns respnse data in JSON format
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,56 +1,97 @@
-import React from 'react'
-import { useState } from 'react';
-import MainPageLayout from '../My Components/MainPageLayout';
-import { apiGet } from '../Misc/config';
+import React from "react";
+import { useState } from "react";
+import MainPageLayout from "../My Components/MainPageLayout";
+import { apiGet } from "../Misc/config";
 
 const Home = () => {
-
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [results, setResults] = useState(null);
+  const [searchOpion, setSearchOpion] = useState("shows");
 
-  const onSearch = () => {   // function to search for the given query
+  const onSearch = () => {
+    // function to search for the given query
 
-    apiGet(`/search/shows?q=${input}`).then(   
-      result => {
-        setResults(result);
-        console.log(result);  // get data from remote API
-      }
-    );
-  }
+    apiGet(`/search/${searchOpion}?q=${input}`).then((result) => {
+      setResults(result);
+      console.log(result); // get data from remote API
+    });
+  };
 
-  const onInputChange  = (event) => {
+  const onInputChange = (event) => {
     setInput(event.target.value);
-  }
+  };
 
   const onKeyDown = (event) => {
-    if (event.keyCode === 13) {   // fires the event only when we press Enter
+    if (event.keyCode === 13) {
+      // fires the event only when we press Enter
       onSearch();
     }
-  }
+  };
+
+  const changeSearchOption = (option) => {
+    setSearchOpion(option);
+  };
+  console.log(searchOpion);
 
   const RenderResults = () => {
     if (results && results.length === 0) {
-      return <div>No Results</div>
+      return <div>No Results</div>;
     }
+    
     if (results && results.length > 0) {
-      return <div>{
-          results.map(element => {
-            return <div key = {element.show.id}>{element.show.name}</div>
+
+      return results[0].show
+        ? results.map((element) => {
+            return <div key={element.show.id}>{element.show.name}</div>;
           })
-        }</div>
+        : results.map((element) => {
+            return <div key={element.person.id}>{element.person.name}</div>;
+          });
     }
+
     return null;
-  }
+  };
 
   return (
-    <div>
+    <div className="container">
       <MainPageLayout>
-        <input type="text" onChange={onInputChange} onKeyDown = {onKeyDown} value = {input} />
-        <button type = "button" onClick={onSearch}>Search</button>
+        <input
+          type="text"
+          onChange={onInputChange}
+          onKeyDown={onKeyDown}
+          value={input}
+        />
+        <div>
+          <label htmlFor="show-search">
+            Shows
+            <input
+              id="show-search"
+              type="radio"
+              value="shows"
+              checked={searchOpion === "shows"}
+              onChange={() => changeSearchOption("shows")}
+            />
+          </label>
+          <label htmlFor="actors-search">
+            Actors
+            <input
+              id="actors-search"
+              type="radio"
+              value="people"
+              checked={searchOpion === "people"}
+              onChange={() => changeSearchOption("people")}
+            />
+          </label>
+        </div>
+
+        <button type="button" onClick={onSearch}>
+          Search
+        </button>
+
         {RenderResults()}
       </MainPageLayout>
     </div>
   );
-}
+};
 
 export default Home;
