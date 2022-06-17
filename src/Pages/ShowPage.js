b7938ebe59086c7ea3d84d69af9541be687b1e7e
Ipsit <ishukr2101@gmail.com>
2022-06-18 01:40:55 +0530

    state management using useReducer

--- src/Pages/ShowPage.js
+++ src/Pages/ShowPage.js
@@ -1,22 +1,61 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { apiGet } from "../Misc/config";
 
-const ShowPage = () => {
-  const { id } = useParams();
-  const [showInfo, setShowInfo] = useState(null);
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+}
 
+const reducer = (prevState, action) => {
+  switch(action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show }
+    }
+    case 'FETCH_UNSUCCESS': {
+      return { ...prevState, isLoading: false, error:  action.error }
+    }
+
+    default: return prevState;
+  }
+}
+
+const ShowPage = () => {
+
+  const { id } = useParams();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log(state);
   useEffect(() => {      // this react hook fires the callback function everytime when anything inside the dependency 
   // array changes
+    let isMounted = true;  // useEffect fires the callback function only after the component is mounted
 
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then((result) => {
-      setShowInfo(result);
-    });
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(result => {
+
+      if (isMounted) {
+        dispatch({ type: 'FETCH_SUCCESS', show: result });
+      }
+      
+    }).catch(err => {
+      if (isMounted) {
+        dispatch({ type: 'FETCH_UNSUCCESS', error: err.message });
+      }
+    })
+    return () => {
+      isMounted = false;
+    }
 
   }, [id]);
-  console.log(showInfo);
 
+  console.log(state.show);
+  if (state.isLoading) {
+    return <div>Data is being loaded</div>
+  }
+  if (state.error) {
+    return <div>Error Ocurred: {state.error}</div>
+  }
   return (
     <div>
       this is show page
